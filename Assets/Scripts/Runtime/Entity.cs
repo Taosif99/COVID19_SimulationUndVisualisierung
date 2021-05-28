9ffdd33dae5dc8614270d8e05f6eacd3d03fc7c3
flac <acun@stud.fra-uas.de>
2021-05-28 17:44:04 +0200

    Add Runtime Classes

---
+++ Assets/Scripts/Runtime/Entity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Runtime
+{
+    class Entity : MonoBehaviour
+    {
+        protected void Initialize(Vector2 position)
+        {
+            gameObject.transform.position = new Vector3(position.x, 0, position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Graph.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Assets.Scripts.Runtime
+{
+    class Graph : Entity
+    {
+
+    }
+}
