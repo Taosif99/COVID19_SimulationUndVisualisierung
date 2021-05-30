5da7e138506266040cbaf83047014b050cfb61f4
TheBlur98 <57642900+TheBlur98@users.noreply.github.com>
2021-05-30 16:06:00 +0200

    Add interpolation for camera movement
    Made camera movement smoother with Unitys Lerp() method.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 /// <summary>
 /// Simple Script which implements scene camera 
 /// movement with wasd und up,down, left right buttons.
+/// 
+/// Good Explanation for Interpolation: http://www.faustofonseca.com/tutorial/unity-vector3-lerp-vs-vector3-slerp
 /// </summary>
 public class CameraMovement : MonoBehaviour
 {
@@ -10,28 +12,31 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private float cameraMovementSpeed = 5;
 
+    //Lower = smoother, Factor used for interpolation
+    [SerializeField] private float _smoothFactor = 0.5f;
 
     // Start is called before the first frame update
     private void Start()
     {
-     //Getting the camera component
-     _mainCamera = GetComponent<Camera>();
+        //Getting the camera component
+        _mainCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-     MoveCamera();
+        MoveCamera();
     }
 
-   /// <summary>
-   /// Method which moves the main Camera in the world.
-   /// </summary>
+    /// <summary>
+    /// Method which moves the main Camera in the world.
+    /// </summary>
     private void MoveCamera()
     {
         //Better later implement an inputHandler / manager
-        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"),0,
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0,
                                             Input.GetAxis("Vertical"));
-        _mainCamera.transform.position += inputVector * Time.deltaTime * cameraMovementSpeed;
+        Vector3 targetPosition = _mainCamera.transform.position + inputVector * Time.deltaTime * cameraMovementSpeed;
+        _mainCamera.transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothFactor);
     }
 }
