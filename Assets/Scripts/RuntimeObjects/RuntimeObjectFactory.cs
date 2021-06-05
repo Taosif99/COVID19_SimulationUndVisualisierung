b3efdd73392df1a008cfe9f9f9826118f87b6e07
flac <acun@stud.fra-uas.de>
2021-06-05 15:01:33 +0200

    Add RuntimeObjectFactory

---
+++ Assets/Scripts/RuntimeObjects/RuntimeObjectFactory.cs
@@ -0,0 +1,23 @@
+﻿using EditorObjects;
+using Simulation.Edit;
+using UnityEngine;
+
+namespace Assets.Scripts.RuntimeObjects
+{
+    /// <summary>
+    /// This Interface shall define all Methods/Properties an RuntimeObject must fulfill.
+    /// </summary>
+    public static class RuntimeObjectFactory
+    {
+        //WIP
+        /*public static Simulation.Runtime.Workplace CreateVenueRuntimeObject(WorkplaceEditorObject workplaceEditorObject)
+        {
+            //The GameObject of the Editor object
+            GameObject RuntimeGameObject = workplaceEditorObject.EditorGameObject;
+
+            
+
+            return 
+        }*/
+    }
+}
