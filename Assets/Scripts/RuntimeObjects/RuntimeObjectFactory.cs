0b47a65a9668431472b2a90dd2131b6a162e7fa5
flac <acun@stud.fra-uas.de>
2021-06-05 16:12:34 +0200

    Further development on Runtime Factory

--- Assets/Scripts/RuntimeObjects/RuntimeObjectFactory.cs
+++ Assets/Scripts/RuntimeObjects/RuntimeObjectFactory.cs
@@ -2,22 +2,63 @@
 using Simulation.Edit;
 using UnityEngine;
 
-namespace Assets.Scripts.RuntimeObjects
+namespace RuntimeObjects
 {
     /// <summary>
     /// This Interface shall define all Methods/Properties an RuntimeObject must fulfill.
     /// </summary>
-    public static class RuntimeObjectFactory
+    static class RuntimeObjectFactory
     {
-        //WIP
-        /*public static Simulation.Runtime.Workplace CreateVenueRuntimeObject(WorkplaceEditorObject workplaceEditorObject)
+        public static Simulation.Runtime.Graph CreateGraphRuntimeObject(GraphEditorObject graphEditorObject)
         {
             //The GameObject of the Editor object
-            GameObject RuntimeGameObject = workplaceEditorObject.EditorGameObject;
+            GameObject graphRuntimeGameObject = graphEditorObject.EditorGameObject;
 
-            
+            //We may add here the required monobehaviours
+            //...gameObject.AddComponent
 
-            return 
-        }*/
+            Simulation.Runtime.Graph graph = new Simulation.Runtime.Graph(graphEditorObject.EditorEntity);
+
+            return graph;
+        }
+
+        public static Simulation.Runtime.Hospital CreateHospitalRuntimeObject(HospitalEditorObject hospitalEditorObject)
+        {
+            //The GameObject of the Editor object
+            GameObject hospitalRuntimeGameObject = hospitalEditorObject.EditorGameObject;
+
+            //We may add here the required monobehaviours
+            //...gameObject.AddComponent
+
+            Simulation.Runtime.Hospital hospital = new Simulation.Runtime.Hospital((Hospital)hospitalEditorObject.EditorEntity);
+
+            return hospital;
+        }
+
+        public static Simulation.Runtime.Household CreateHouseholdRuntimeObject(HouseholdEditorObject householdEditorObject)
+        {
+            //The GameObject of the Editor object
+            GameObject householdRuntimeGameObject = householdEditorObject.EditorGameObject;
+
+            //We may add here the required monobehaviours
+            //...gameObject.AddComponent
+
+            Simulation.Runtime.Household household = new Simulation.Runtime.Household((Household)householdEditorObject.EditorEntity);
+
+            return household;
+        }
+
+        public static Simulation.Runtime.Workplace CreateWorkplaceRuntimeObject(WorkplaceEditorObject workplaceEditorObject)
+        {
+            //The GameObject of the Editor object
+            GameObject workplaceRuntimeGameObject = workplaceEditorObject.EditorGameObject;
+
+            //We may add here the required monobehaviours
+            //...gameObject.AddComponent
+
+            Simulation.Runtime.Workplace workplace = new Simulation.Runtime.Workplace((Workplace)workplaceEditorObject.EditorEntity);
+
+            return workplace;
+        }
     }
 }
--- Assets/Scripts/Simulation/Runtime/Household.cs
+++ Assets/Scripts/Simulation/Runtime/Household.cs
@@ -1,27 +1,23 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Simulation.Runtime
 {
     class Household : Venue
     {
-        private Person[] _members;
-
         public Household(Edit.Household editorEntity) : base(editorEntity)
         {
-            NumberOfPeople = editorEntity.NumberOfPeople;
-            PercentageOfWorkers = editorEntity.PercentageOfWorkers;
-            CarefulnessTendency = editorEntity.CarefulnessTendency;
-            RiskTendency = editorEntity.RiskTendency;
-            NumberOfShoppingRuns = editorEntity.NumberOfShoppingRuns;
-            NumberOfShoppers = editorEntity.NumberOfShoppers;
+            int numberOfWorkers = Mathf.CeilToInt(editorEntity.PercentageOfWorkers * editorEntity.NumberOfPeople);
+
+            Members = new Person[editorEntity.NumberOfPeople];
+
+            for (int i = 0; i < editorEntity.NumberOfPeople; i++)
+            {
+                Person person = new Person(editorEntity.CarefulnessTendency, editorEntity.RiskTendency, i < numberOfWorkers);
+                Members[i] = person;
+            }
         }
 
-        public Person[] members { get => _members; set => _members = value; }
-        public byte NumberOfPeople { get; set; }
-        public float PercentageOfWorkers { get; set; }
-        public float CarefulnessTendency { get; set; }
-        public float RiskTendency { get; set; }
-        public int NumberOfShoppingRuns { get; set; }
-        public int NumberOfShoppers { get; set; }
+        public Person[] Members { get; }
     }
 }
