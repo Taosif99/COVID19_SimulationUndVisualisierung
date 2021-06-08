6cf77470e7920b53d24314f6b669b27c9c6d33c9
reza-dot <58373676+reza-dot@users.noreply.github.com>
2021-06-08 16:51:58 +0200

    add UpdateInfectionState method to Person

--- Assets/Scripts/TestScripts/PersonUpdateInfectionStateTest.cs
+++ Assets/Scripts/TestScripts/PersonUpdateInfectionStateTest.cs
@@ -13,44 +13,51 @@ namespace TestScripts
 
         public List<Person> TestPopulation = new List<Person>();
 
-        //https://docs.microsoft.com/de-de/dotnet/api/system.datetime?view=net-5.0
-        //Tag heute
-        public DateTime  currentDay = new DateTime();
-
+        DateTime today = DateTime.Today;
         // Start is called before the first frame update
         void Start()
         {
 
-            Debug.Log("Testbegin");
+            /*
+            int currentMonth = UnityEngine.Random.Range(1, 12);
+            currentDate = currentDate.AddDays(currentMonth);
 
+            int currentDay = UnityEngine.Random.Range(1, 30);
+            currentDate = currentDate.AddMonths(currentDay);
+            */
+            /*
+            Debug.Log(today);
+            Debug.Log("Current Day:" + today);
             //init 30 persons
-            for (int i = 0; i < 10; i++)
+            
+            for (int i = 0; i < 5; i++)
             {
 
                 float randomCarefulnessFactor = UnityEngine.Random.Range(0f, 1f);
                 float randomRisk = UnityEngine.Random.Range(0f, 1f);
                 Person person = new Person(randomCarefulnessFactor, randomRisk, true);
 
-                //assign random dateTime since infection
-                // eg. days of current mounth
-
-
+                person._infectionDate = new DateTime(2021, UnityEngine.Random.Range(5,7), UnityEngine.Random.Range(1, today.Day));
+                Debug.Log("Infections date: " + person._infectionDate);
                 TestPopulation.Add(person);
+
             }
 
             //Todo implement 
-
+            
             foreach (Person person in TestPopulation)
             {
-                person.UpdateInfectionState();
+                person._infectionStates = Person.InfectionStates.Infected | Person.InfectionStates.Infectious;
+                person.UpdateInfectionState(today);    
             }
+            */
+   
+
+
 
         }
 
         // Update is called once per frame
-        void Update()
-        {
-
-        }
+       
     }
 }
