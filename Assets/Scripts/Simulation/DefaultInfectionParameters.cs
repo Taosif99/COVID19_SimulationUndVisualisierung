298bc6775dd089bea7e588bd4f0f0c6aabc7d9e4
TheBlur98 <57642900+TheBlur98@users.noreply.github.com>
2021-06-27 13:02:32 +0200

    Add namespace to infection parameters and add tool tips for simulation settings.

---
+++ Assets/Scripts/Simulation/DefaultInfectionParameters.cs
@@ -0,0 +1,71 @@
+namespace Simulation
+{
+    public static class DefaultInfectionParameters
+    {
+        /// <summary>
+        /// This class stores default infections values for the simulation.
+        /// The average values are taken from:
+        /// <see cref="https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Modellierung_Deutschland.pdf;jsessionid=6BEA5234400C372CBCA61A2F969FA97D.internet062?__blob=publicationFile"/>
+        /// <see cref="https://www.sciencemediacenter.de/alle-angebote/fact-sheet/details/news/verlauf-von-covid-19-und-kritische-abschnitte-der-infektion/"/>
+        /// </summary>
+        public static class InfectionsPhaseParameters
+        {
+
+            //Following values are the averages of the RKI
+
+            /// <summary>
+            /// Amount days until a person becomes infectious.
+            /// </summary>
+            public const int LatencyTime = 3;
+
+            /// <summary>
+            /// Amount days a person IS infectious.
+            /// </summary>
+            public const int AmountDaysInfectious = 10;
+
+            public const int EndDayInfectious = LatencyTime + AmountDaysInfectious - 1;
+
+            //Incubation Time, whether use the median or generate a value
+
+            /// <summary>
+            /// Amount days until a person gets symptoms.
+            /// </summary>
+            public const int IncubationTime = 5;
+
+            /// <summary>
+            /// Amount days a person HAS symptoms. (If person must not go to an hospital)
+            /// </summary>
+            public const int AmountDaysSymptoms = 9;
+
+            public const int EndDaySymptoms = IncubationTime + AmountDaysSymptoms - 1;
+
+
+        }
+
+        /// <summary>
+        /// TODO USE IN HOSPITAL
+        /// </summary>
+        public static class HealthPhaseParameters
+        {
+
+            /// <summary>
+            /// Probability that an infected person recovers,
+            /// else person must go to hospital !
+            /// </summary>
+            public const float RecoveringProbability = 0.955f;
+            /// <summary>
+            /// Probability that a person does recover in hospital,
+            /// else person must go to intensive care.
+            /// </summary>
+            public const float RecoveringInHospitalProbability = 0.75f;
+
+            /// <summary>
+            /// Person dies, if he/she does not survive in the intensive care.
+            /// </summary>
+            public const float PersonSurvivesIntensiveCareProbability = 0.5f;
+
+
+        }
+
+    }
+}
