04407dac31f568e4da005173e6276a146eb1c27b
TheBlur98 <57642900+TheBlur98@users.noreply.github.com>
2021-07-01 18:30:22 +0200

    Typo fix

--- Assets/Scripts/Simulation/Runtime/Venue.cs
+++ Assets/Scripts/Simulation/Runtime/Venue.cs
@@ -29,7 +29,7 @@ namespace Simulation.Runtime
 
                 foreach (Person i in _currentPeopleAtVenue)
                 {
-                    if (!i.InfectionState.HasFlag(Person.InfectionStates.Infectious) || p.IsInHospitalization)
+                    if (!i.InfectionState.HasFlag(Person.InfectionStates.Infectious) || i.IsInHospitalization)
                     {
                         continue;
                     }
